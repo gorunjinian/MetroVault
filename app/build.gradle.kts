9094661fc7eac3147c90db293de097a0d2dbcfad
Gorun Jinian <gorunraffijinian@gmail.com>
2025-12-09 09:24:27 +0200

    v.2.0.0 config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.gorunjinian.metrovault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.5.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
